91c4881422495be08e795330409286f0063017f8
nakaharabo <77267456+nakaharabo@users.noreply.github.com>
2021-04-02 10:11:05 +0900

    商品編集機能の修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_params, only:[:show, :edit, :update]
+  before_action :redirect_root, only:[:edit, :update]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -24,11 +25,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-
     if @item.update(item_params)
       redirect_to item_path,method: :patch  
    else
@@ -46,5 +45,9 @@ class ItemsController < ApplicationController
 
   def find_params
     @item = Item.find(params[:id])
+  end
+
+  def redirect_root
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
